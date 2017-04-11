b141c940ed04bdfb66b57749a57fe490ca8466b0
KRISHNA\CHANDAN <chandan115325@gmail.com>
2017-04-11 15:48:15 +0530

    java_assignment_7.2 is done.

---
+++ src/SubStringExample.java
@@ -0,0 +1,13 @@
+// a program to find a substring in a String by using an inbuilt method of String class.
+public class SubStringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// String variable s is declared and initialized 
+		String s = "chandan is genius";
+		// substring is retrieved into new String variable sub and displayed on console.
+		String sub = s.substring(0, 7);
+		System.out.println("The substring output is :" +sub);
+	}
+
+}
